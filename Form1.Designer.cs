a9500ae3a715bcb3954b6caba4f517f9f2fb0f22
Matt Weller <matthew.weller@nvih.org>
2019-09-30 16:53:47 -0700

    documentation and bugfixes
    * add documentation explaining weigand conversion
    * bugfix for if binary string ends up only 23 chars
    * add tooltip for copying doorcode to clipboard

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,14 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(45, 54);
+            this.textBox1.Location = new System.Drawing.Point(15, 35);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(214, 20);
             this.textBox1.TabIndex = 0;
@@ -44,7 +47,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(42, 38);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(109, 13);
             this.label1.TabIndex = 1;
@@ -53,22 +56,36 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(45, 110);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 101);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.Size = new System.Drawing.Size(0, 29);
             this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Winpak door code:";
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.IsBalloon = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(464, 233);
+            this.ClientSize = new System.Drawing.Size(400, 181);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Door Code Converter";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,6 +96,9 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ToolTip toolTip1;
+        
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,15 +15,36 @@ namespace Door_Code_Converter
         public Form1()
         {
             InitializeComponent();
+            toolTip1.SetToolTip(label2, "Double click to copy to clipboard!");
         }
 
         private void TextBox1_TextChanged(object sender, EventArgs e)
         {
+            /*
+                1. we start with a 10-digit base10 integer: for example, 0007956654.
+                2. we convert that to binary: 11110010110100010101110.
+                3. strip the first 8 (that's the facility code section; we don't use it). it becomes 110100010101110. 
+                4. convert that back to decimal. finally, the door code for badge 0007956654 is 26798.
+
+                see https://www.getkisi.com/blog/how-to-calculate-facility-code-using-card-bit-calculators for solid explanation
+             */
             if (textBox1.Text.Length == 10) {
-                // create binary string from input numbers - only the last 16 digits
-                string s = Convert.ToString(Int32.Parse(textBox1.Text), 2).Substring(8);
-                // convert binary string to Int32
-                label2.Text = Convert.ToInt32(s, 2).ToString();
+                // create binary string from input numbers - only the last 16 digits. this is step 2
+                string s24 = Convert.ToString(Int32.Parse(textBox1.Text), 2);
+                // depending on the number, it might omit a leading zero - ensure that's here so conversions are always consistently done on 24-bit strings.
+                if (s24.Length < 24) {
+                    s24 = "0" + s24;
+                }
+                //MessageBox.Show(s24.ToString() + " - length: " + s24.Length.ToString());
+                string s16 = s24.Substring(8); // slice off the first 8 - step 3
+                // convert binary string back to decimal (int32). step 4.
+                label2.Text = Convert.ToInt32(s16, 2).ToString();
+                label2.Visible = true;
+            }
+            else
+            {
+                label2.Text = "";
+                label2.Visible = false;
             }
         }
     }
