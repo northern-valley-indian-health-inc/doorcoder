02a04bf66919808d4dd693e785391311c85c8d2b
Matt Weller <matthew.weller@nvih.org>
2019-09-30 15:29:29 -0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Door_Code_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 10) {
+                // create binary string from input numbers - only the last 16 digits
+                string s = Convert.ToString(Int32.Parse(textBox1.Text), 2).Substring(8);
+                // convert binary string to Int32
+                label2.Text = Convert.ToInt32(s, 2).ToString();
+            }
+        }
+    }
+}
